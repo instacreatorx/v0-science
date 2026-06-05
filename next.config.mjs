f534d240856cbd59bbdfa7632b73cef811c64785
Your Name <you@example.com>
2026-06-05 12:20:47 +0330

    distDir: ".moz",
      // 2. Tells Next.js to generate a self-contained production bundle
      output: "standalone",

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,9 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
+  }, 
+  // 2. Tells Next.js to generate a self-contained production bundle
+  output: "standalone",
 };
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
